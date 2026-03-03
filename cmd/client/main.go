3a5f1523b2a5ab8f0ebcb41a78c9670be6706afd
megautam18 <prabhu.gautam14@gmail.com>
2026-03-03 22:17:35 +0530

    Added cursor functionality to display current position

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"strings"
+	"time"
 
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
 )
 
 type model struct {
-	info       []string
-	targetText string
-	typedText  string
+	info          []string
+	targetText    string
+	typedText     string
+	visibleCursor bool
+}
+
+type cursorBlink struct{}
+
+func blink() tea.Cmd {
+	return tea.Tick(time.Duration(500)*time.Millisecond, func(t time.Time) tea.Msg {
+		return cursorBlink{}
+	})
 }
 
 func initModel() model {
 	return model{
-		info:       []string{"PunchKeys", "Press ctrl+b to quit"},
-		targetText: "the quick brown fox jumps over the lazy dog",
-		typedText:  "",
+		info:          []string{"PunchKeys", "Press ctrl+b to quit"},
+		targetText:    "the quick brown fox jumps over the lazy dog",
+		typedText:     "",
+		visibleCursor: true,
 	}
 }
 func (m model) Init() tea.Cmd {
-	return nil
+	return blink()
 }
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
@@ -45,22 +56,30 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if checker(m) {
 			return m, tea.Quit
 		}
+	case cursorBlink:
+		m.visibleCursor = !m.visibleCursor
+		return m, blink()
 	}
+
 	return m, nil
 }
 func (m model) View() tea.View {
 	s := strings.Join(m.info, "\n")
-	s += "\n" + rendertext(m.targetText, m.typedText)
+	s += "\n" + rendertext(m.targetText, m.typedText, m.visibleCursor)
 	return tea.NewView(s)
 }
-func rendertext(targetText string, typedText string) string {
+func rendertext(targetText string, typedText string, visible bool) string {
 	correct := lipgloss.NewStyle().Foreground(lipgloss.Color("2"))
 	incorrect := lipgloss.NewStyle().Foreground(lipgloss.Color("1"))
 	extra := lipgloss.NewStyle().Foreground(lipgloss.Color("1")).Faint(true)
 	standard := lipgloss.NewStyle().Foreground(lipgloss.Color("0")).Faint(true)
 	var result strings.Builder
 	maxlen := max(len(targetText), len(typedText))
+	curpos := len(typedText)
 	for i := 0; i < maxlen; i++ {
+		if i == curpos && visible {
+			result.WriteString("|")
+		}
 		if i >= len(targetText) && i < len(typedText) {
 			result.WriteString(extra.Render(string(typedText[i])))
 			continue
@@ -76,6 +95,9 @@ func rendertext(targetText string, typedText string) string {
 			ch := rune(targetText[i])
 			result.WriteString(standard.Render(string(ch)))
 		}
+	}
+	if curpos == maxlen && visible {
+		result.WriteString("|")
 	}
 	return result.String()
 }
